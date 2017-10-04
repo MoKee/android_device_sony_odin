87e2d6a93513e2ba98da49b144e005f78c831071
martincz <martincz@mokeedev.com>
2017-10-04 19:27:58 +0800

    odin: mokee-ify
    Change-Id: I73a0433d2a46f5a912b72e842ae439ab27adcf77

--- mokee.mk
+++ mokee.mk
@@ -15,8 +15,8 @@
 # limitations under the License.
 #
 
-# Inherit common LineageOS stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common MK stuff
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/sony/odin/device.mk)
@@ -26,7 +26,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifications
-PRODUCT_NAME := lineage_odin
+PRODUCT_NAME := mk_odin
 PRODUCT_DEVICE := odin
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
